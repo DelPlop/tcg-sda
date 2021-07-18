ac0e16f11cebc3b352a3bc90e784bcbfc86020a2
DelPlop <github@phpblocnote.net>
2021-07-18 19:56:24 +0200

    liste des éditions

---
+++ src/Controller/EditionController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Edition;
+use App\Repository\EditionRepository;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class EditionController extends AbstractController
+{
+    public function editions(EditionRepository $editionRepository, bool $onlyVisible = true): Response
+    {
+        return $this->render('edition/left.html.twig', [
+            'editions' => $editionRepository->findBy([], ['editionNumber' => 'asc'])
+        ]);
+    }
+
+    public function cards(Edition $edition): Response
+    {
+        return $this->render('edition/cards.html.twig', [
+            'activePage' => 'editions',
+            'edition' => $edition,
+            'cards' => $edition->getCards()
+        ]);
+    }
+
+    public function list(EditionRepository $editionRepository): Response
+    {
+        return $this->render('edition/list.html.twig', [
+            'activePage' => 'editions',
+            'editions' => $editionRepository->findBy([], ['editionNumber' => 'asc'])
+        ]);
+    }
+}
