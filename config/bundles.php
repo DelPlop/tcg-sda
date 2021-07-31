563eac525c70871727bdacee4717978a5ce5091c
DelPlop <github@phpblocnote.net>
2021-07-31 07:40:11 +0200

    mot de passe oublié

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,5 @@ return [
     DelPlop\UserBundle\DelPlopUserBundle::class => ['all' => true],
     Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
+    SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
 ];
