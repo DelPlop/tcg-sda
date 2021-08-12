c8c02ffac6a5252f781423297cd04eb9bbd67397
DelPlop <github@phpblocnote.net>
2021-08-12 18:28:08 +0200

    mise à jour dernière connexion

--- src/Controller/StaticController.php
+++ src/Controller/StaticController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Security;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 class StaticController extends AbstractController
 {
@@ -28,8 +30,16 @@ class StaticController extends AbstractController
         ]);
     }
 
-    public function topRight(bool $withSearch = true): Response
+    public function topRight(Security $security, bool $withSearch = true): Response
     {
+        if ($security->isGranted('IS_AUTHENTICATED_REMEMBERED') && $security->getUser() instanceof UserInterface) {
+            $security->getUser()->setUpdatedAt(new \DateTime('now'));
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($security->getUser());
+            $entityManager->flush();
+        }
+
         return $this->render('static/topRight.html.twig', [
             'withSearch' => $withSearch
         ]);
